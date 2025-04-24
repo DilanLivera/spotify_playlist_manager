45412201194d1ad0ad1c6571289011484e2feff0
Dilan Livera <dilan_yahampath@yahoo.com>
2025-04-24 22:02:44 +1000

    feat(ui): copy template

--- src/UI/Program.cs
+++ src/UI/Program.cs
@@ -1,14 +1,42 @@
 using UI.Components;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 
 WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
+builder.Services
+       .AddRazorComponents()
        .AddInteractiveServerComponents();
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services
+       .AddAuthentication(options =>
+       {
+           options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+           options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+       })
+       .AddCookie()
+       .AddGoogle(options =>
+       {
+           builder.Configuration.Bind(key: "Authentication:Google", options);
+
+           if (string.IsNullOrEmpty(options.ClientId))
+           {
+               throw new InvalidOperationException("Google ClientId not found.");
+           }
+
+           if (string.IsNullOrEmpty(options.ClientSecret))
+           {
+               throw new InvalidOperationException("Google ClientSecret not found.");
+           }
+       });
+
+builder.Services.AddAuthorizationCore();
 
 WebApplication? app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
@@ -20,6 +48,30 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet(
+           pattern: "/signin",
+           async (HttpContext context) =>
+           {
+               AuthenticationProperties properties = new() { RedirectUri = "/" };
+               await context.ChallengeAsync(
+                                            GoogleDefaults.AuthenticationScheme,
+                                            properties);
+
+               return Results.Empty;
+           });
+
+app.MapGet(
+           pattern: "/signout",
+           async (HttpContext context) =>
+           {
+               await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+               return Results.Redirect(url: "/");
+           });
 
 app.MapRazorComponents<App>()
    .AddInteractiveServerRenderMode();
